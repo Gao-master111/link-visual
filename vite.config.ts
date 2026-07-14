a476e3e8209eda49469ddcadb22b4464426260b1
Gao-master111 <1989150858@qq.com>
2026-07-14 20:23:47 +0800

    修复github pages二级路径，新增404路由兼容

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// 判断是否GitHub Pages打包
 const isGithubPages = process.env.GITHUB_PAGES === 'true'
 
 export default defineConfig({
   plugins: [vue()],
-  // 线上用/link-visual/，本地用/
   base: isGithubPages ? '/link-visual/' : '/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
   build: {
     assetsDir: 'assets',
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
