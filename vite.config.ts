3337b94d5d2df736a0bb4d0424131c4ddc31dc53
Gao-master111 <1989150858@qq.com>
2026-07-14 18:46:40 +0800

    修复部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/link-visual/',  // 和仓库名完全一致，前后都有斜杠
   server: {
-    host: '0.0.0.0', // 关键：监听所有网卡，允许局域网访问
-    port: 5173,
-    open: false
+    host: '0.0.0.0',
+    port: 5173
   }
 })
